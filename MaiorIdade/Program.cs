081492abaaf79726aba539a7b7b6c24de91645a6
victorbrandaao <victorbrandaotech@gmail.com>
2024-11-04 10:44:31 -0300

    Initial commit

---
+++ MaiorIdade/Program.cs
@@ -0,0 +1,23 @@
+﻿// Crie um programa que leia o nome e a idade de 3 alunos e mostre o nome do aluno mais velho.
+
+String aluno1 = "João";
+String aluno2 = "Maria";
+String aluno3 = "José";
+int idade1 = 20;
+int idade2 = 21;
+int idade3 = 22;
+
+int maiorIdade = idade1;
+String alunoMaisVelho = aluno1;
+
+if (idade2 > maiorIdade) {
+    maiorIdade = idade2;
+    alunoMaisVelho = aluno2;
+}
+
+if (idade3 > maiorIdade) {
+    maiorIdade = idade3;
+    alunoMaisVelho = aluno3;
+}
+
+Console.WriteLine($"O aluno mais velho é {alunoMaisVelho} com {maiorIdade} anos.");
